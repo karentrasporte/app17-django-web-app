61d72015aa825b6e4e12255159fbf5ff13763c69
karentrasporte <karentrasporte@gmail.com>
2026-04-17 15:02:32 +0800

    Added code to save data to db and added an admin view

--- job_application/models.py
+++ job_application/models.py
@@ -4,7 +4,7 @@ class Form(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
-    birth_date = models.DateField()
+    date = models.DateField()
     occupation = models.CharField(max_length=100)
 
     def __str__(self):
--- job_application/views.py
+++ job_application/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
+
 
 def index(request):
     if request.method == "POST":
         form = ApplicationForm(request.POST)
+        print(form.is_valid())
         if form.is_valid():
             first_name = form.cleaned_data["first_name"]
             last_name = form.cleaned_data["last_name"]
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
+
+            message_body = f"Thanks for your application, {first_name}"
+            email_message = EmailMessage("Submission confirmation", message_body, to=[email])
+            email_message.send()
+
+            messages.success(request, "Thank you for your application!")
 
 
     return render(request, "index.html")
